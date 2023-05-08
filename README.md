71884f83749ecfa8218967b403e5147b49441d5f
Charles Lowell <cowboyd@frontside.com>
2023-05-08 10:06:49 -0500

    try implementing a custom JSX transform

---
+++ README.md
@@ -0,0 +1,7 @@
+## deno-jsx
+
+This attempts to define a custom JSX transform using a local module. Try it with:
+
+``` text
+$ deno run example.tsx
+```
---
+++ example.tsx
@@ -0,0 +1,11 @@
+/** @jsxImportSource ./custom */
+
+export function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+console.dir({ App });
